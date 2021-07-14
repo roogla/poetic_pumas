f92ca0a1771f8831054db87f1ef876793b37086e
Thomas Arambula <thomasarambula96@gmail.com>
2021-07-13 17:17:12 -0700

    Add better emoji for block dude. Refactors code for readability and improves game control.

--- src/elements/blockdude.py
+++ src/elements/blockdude.py
@@ -9,34 +9,42 @@ class BlockDude(LevelElement):
     """Block Dude himself. The main character."""
 
     level_symbol = "D"
-    string_symbol = "Á"
+    string_symbol = "🤷"
+
+    def move_laterally(self, data: ElementData, spaces: int = 1) -> None:
+        """Moves this level element laterally by some number of spaces given.
+
+        The number of spaces can be positive or negative."""
+        position = data.level.get_element_position(self)
+        lateral_position = position + Position(spaces, 0)
+        lateral_element = data.level.get_element_at_position(lateral_position)
+        destination = lateral_position
+
+        if not is_space_element(lateral_element):
+            # the element directly up and laterally
+            one_up_and_lateral_position = position + Position(spaces, -1)
+            one_up_lateral_element = data.level.get_element_at_position(
+                one_up_and_lateral_position
+            )
+
+            # if there's a block above the one in the way
+            if not is_space_element(one_up_lateral_element):
+                return
+            destination = one_up_and_lateral_position
+
+        data.level.move_element(position, destination)
+        data.renderer.render_level(data.level)
 
     def move_left(self, data: ElementData) -> None:
         """Move this level element leftward according to the rules.
 
         If there are any objects to the left of block dude, he cannot move left.
         """
-        position = data.level.get_element_position(self)
-        one_unit_leftward = position + Position(-1, 0)
-        left_element = data.level.get_element_at_position(one_unit_leftward)
-
-        if not is_space_element(left_element):
-            return
-
-        data.level.move_element(position, one_unit_leftward)
-        data.renderer.update_level_render(data.level)
+        self.move_laterally(data, spaces=-1)
 
     def move_right(self, data: ElementData) -> None:
         """Move this level element leftward according to the rules.
 
         If there are any objects to the left of block dude, he cannot move left.
         """
-        position = data.level.get_element_position(self)
-        one_unit_rightward = position + Position(1, 0)
-        left_element = data.level.get_element_at_position(one_unit_rightward)
-
-        if not is_space_element(left_element):
-            return
-
-        data.level.move_element(position, one_unit_rightward)
-        data.renderer.update_level_render(data.level)
+        self.move_laterally(data, spaces=1)
