8b4f7f751836d79a9f9dcf7d6f008aab6e7fe81f
Thomas Arambula <thomasarambula96@gmail.com>
2021-07-14 09:45:00 -0700

    Expand type hinting capabilities of vector class.

--- src/Vector2D.py
+++ src/Vector2D.py
@@ -1,40 +1,47 @@
 from __future__ import annotations
 
-from math import acos, sqrt
+from math import acos, inf, sqrt
 from typing import Union
+
+Numerics = Union[float, int]
 
 
 class Vector2D:
     """2 Dimensional vector math class. Implements vector operations."""
 
-    def __init__(self, x: int = 0, y: int = 0):
+    def __init__(
+        self, x: Numerics = 0, y: Numerics = 0, scalar_limit: Union[Numerics, None] = None
+    ):
         self._x = x
         self._y = y
-        self._limit: Union[float, int, None] = None
+        # The magnitude of the scalar limit
+        if scalar_limit is None:
+            self.scalar_limit: Numerics = inf
+        else:
+            self.scalar_limit: Numerics = scalar_limit
+
+    def __repr__(self) -> str:
+        return f"{self.__class__.__name__}({self.x},{self.y})"
 
     @property
-    def x(self) -> int:
+    def x(self) -> Numerics:
         """Get x"""
         return self._x
 
     @property
-    def y(self) -> int:
+    def y(self) -> Numerics:
         """Get y"""
         return self._y
 
     @x.setter
-    def x(self, value: int) -> None:
+    def x(self, value: Numerics) -> None:
         """Set x"""
         self._x = value
 
     @y.setter
-    def y(self, value: int) -> None:
+    def y(self, value: Numerics) -> None:
         """Set y"""
         self._y = value
-
-    def __repr__(self) -> str:
-        """repr method for class"""
-        return f"{self.__class__.__name__}({self.x},{self.y})"
 
     def add(self, other: Vector2D) -> None:
         """
@@ -62,7 +69,7 @@ class Vector2D:
 
     def sub(self, other: Vector2D) -> None:
         """
-        Vector subtraction
+        Vector subtraction in place
 
         Subtracts the other vector from instance vector
         in vector form A - B
@@ -84,22 +91,24 @@ class Vector2D:
         new_vector.sub(other)
         return new_vector
 
-    def mul(self, scalar: int) -> None:
+    def mul(self, scalar: Numerics) -> None:
         """
-        Vector multiplication
+        Vector multiplication in place
 
         scales the vector by given scalar or to the max limit defined by
         Vector2D.limit() function
         in vector form A * n
         :param scalar: the scale multiplier
         """
-        if self._limit is not None:
-            scalar = max(self._limit, scalar)
+        if abs(scalar) > self.scalar_limit:
+            # the sign for the scalar (+1 or -1)
+            sign = abs(scalar) // scalar
+            scalar = sign * self.scalar_limit
 
         self.x *= scalar
         self.y *= scalar
 
-    def __mul__(self, scalar: int) -> Vector2D:
+    def __mul__(self, scalar: Numerics) -> Vector2D:
         """
         Operator Vector multiplication
 
@@ -114,9 +123,9 @@ class Vector2D:
         new_vector.mul(scalar)
         return new_vector
 
-    def div(self, scalar: Union[int, float]) -> None:
+    def div(self, scalar: Numerics) -> None:
         """
-        Vector division
+        Vector division in place
 
         scales the vector by given scalar
         in vector form A / n
@@ -163,15 +172,15 @@ class Vector2D:
 
     def limit(self, limit: int) -> None:
         """
-        Limit vector
+        Limit vector magnitude
 
         limits the vector to an upperbound
         :param limit: the limit to be set on vector
         """
-        self._limit = limit
-        self.set_mag(self._limit)
+        self.scalar_limit = limit
+        self.set_mag(self.scalar_limit)
 
-    def dot(self, other: Vector2D) -> int:
+    def dot(self, other: Vector2D) -> Numerics:
         """
         Dot product
 
@@ -182,11 +191,11 @@ class Vector2D:
         """
         return self.x * other.x + self.y * other.y
 
-    def cross(self, other: Vector2D) -> int:
+    def cross(self, other: Vector2D) -> Numerics:
         """
-        Cross product
+        z-component cross product
 
-        cross product of two vectors
+        z-component of cross product of two vectors
         in vector form of A × B
         :param other: other 2D vector
         :return: value of A × B
