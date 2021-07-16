918b87e6151c980a776acaab2f407e2516bd89b6
Jaavv <mjaavv@gmail.com>
2021-07-16 04:47:48 -0600

    added level argv for temporary level debugging

--- src/main.py
+++ src/main.py
@@ -4,9 +4,13 @@ from .game import GameState
 from .level import create_level_from_file
 from .renderer import Renderer
 
+import os
+import sys
+
 # Timeout speed for inputs
 TIMEOUT = 0.1
 
+levels_folder = os.listdir(path='src/resources/levels/')
 
 def main(terminal: Terminal) -> None:
     """Main entry point and loop for the game."""
@@ -14,8 +18,12 @@ def main(terminal: Terminal) -> None:
     # Docker's terminal defaults to 8 colors
     terminal.number_of_colors = 1 << 24
 
+    starting_level = "level-1"
+    if len(sys.argv) >= 2:
+        if f'{sys.argv[1]}.txt' in levels_folder:
+            starting_level = str(sys.argv[1])
     # initalize game vars
-    level = create_level_from_file("./resources/levels/level-1.txt")
+    level = create_level_from_file(f"./resources/levels/{starting_level}.txt")
     renderer = Renderer(terminal=terminal, level=level)
     gamestate = GameState(level=level, renderer=renderer)
 
