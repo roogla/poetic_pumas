569da3038407d78fec255cd9c5f980c1109cf82d
mjfernez <mjfernez@gmail.com>
2021-07-16 22:00:07 -0400

    Fix test-sounds
    Fixes the test-sounds.py example to use the soundboard, fixes relative
    import issue as well, by moving the sys.path.append line before the
    import

--- src/sounds/test-sounds.py
+++ src/sounds/test-sounds.py
@@ -1,5 +1,7 @@
+# Must be run from parent directory: poetic pumas
 import os
 import sys
+os.environ["PYGAME_HIDE_SUPPORT_PROMPT"] = "hide"
 import time
 from random import choice
 from string import ascii_lowercase as KEYS
@@ -8,18 +10,9 @@ from typing import Dict
 from blessed import Terminal
 from pygame import mixer
 
-from src.game.soundboard import Soundboard
-
 # adapted from https://stackoverflow.com/questions/16480898
-os.environ["PYGAME_HIDE_SUPPORT_PROMPT"] = "hide"
 sys.path.append(os.path.join(os.path.dirname(__file__), "..", ".."))
-
-
-def close() -> None:
-    """Closes the terminal and clears the screen"""
-    print(term.clear)
-    mixer.quit()
-    sys.exit(0)
+from src.game.soundboard import Soundboard
 
 
 def draw(x: int, y: int, char: str) -> None:
@@ -36,7 +29,7 @@ def load_sounds() -> Dict[str, Dict[str, mixer.Sound]]:
     the file and the corresponding mixer.Sound object
     """
     sounds = [
-        {"name": x, "sound": mixer.Sound(os.path.join(Soundboard.SFX_DIR, x))}
+            {"name": x[:-4], "sound": mixer.Sound(os.path.join(Soundboard.SFX_DIR, x))}
         for x in sorted(os.listdir(Soundboard.SFX_DIR))
         if x.endswith(".wav")
     ]
@@ -53,9 +46,10 @@ def load_sounds() -> Dict[str, Dict[str, mixer.Sound]]:
 
 if __name__ == "__main__":
     term = Terminal()
-    mixer.init()
+    s = Soundboard()
     keymap = load_sounds()
     with term.cbreak(), term.hidden_cursor():
+        s.play_music("blockdude")
         # clear the screen
         print(term.home + term.clear)
         x, y = 0, 0
@@ -75,10 +69,10 @@ if __name__ == "__main__":
             try:
                 inp = repr(term.inkey()).strip("'")
             except KeyboardInterrupt:
-                close()
+                sys.exit(0)
             if inp in keymap.keys() and (
                 inp != last_inp or (time.time() - last_inp_time > wait_time)
             ):
-                mixer.find_channel(True).play(keymap[inp]["sound"])
+                s.play_sfx(keymap[inp]['name']) 
             last_inp = inp
             last_inp_time = time.time()
