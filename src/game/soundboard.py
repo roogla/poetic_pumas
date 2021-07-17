8cedd3d20beafd63b390149cc7b083d15da5606f
mjfernez <mjfernez@gmail.com>
2021-07-16 20:30:16 -0400

    Adds music loop to the soundboard class
    This commit adds a simple music file (made with musescore and gxscc) to
    be played in the background during a game. This commit also moves the
    logic for quitting the mixer into the del method so the mixer will stop
    on garbage collection

--- src/game/soundboard.py
+++ src/game/soundboard.py
@@ -1,4 +1,6 @@
 import os
+
+os.environ["PYGAME_HIDE_SUPPORT_PROMPT"] = "hide"
 from typing import Dict, List
 
 from pygame import mixer
@@ -8,11 +10,17 @@ class Soundboard:
     """Creates a Soundboard object which controls sounds used in the game"""
 
     SFX_DIR = os.path.join(".", "src", "sounds", "sfx")
+    MUSIC_DIR = os.path.join(".", "src", "sounds", "music")
 
     def __init__(self):
         # TODO: self.sfx, self.music =
         mixer.init()
-        self.sfx = self.load_sounds()
+        self.sfx, self.music = self.load_sounds()
+        self.play_music("blockdude")
+
+    def __del__(self):
+        """Quit the soundboard mixer."""
+        mixer.quit()
 
     def load_sounds(self) -> Dict[str, mixer.Sound]:
         """Loads all sounds from the default sound directories SFX_DIR and MUSIC_DIR
@@ -20,14 +28,19 @@ class Soundboard:
         :return: a dictionary of mixer.Sound objects sorted by filename (no
         extension)
         """
-        # TODO: implement music, reserve channels
-        # mixer.set_reserved(0)
+        # reserves one channel, does not reserve channel 1
+        mixer.set_reserved(1)
         sfx = {
             x[:-4]: mixer.Sound(os.path.join(self.SFX_DIR, x))
             for x in os.listdir(self.SFX_DIR)
             if x.endswith(".wav")
         }
-        return sfx
+        music = {
+            x[:-4]: mixer.Sound(os.path.join(self.MUSIC_DIR, x))
+            for x in os.listdir(self.MUSIC_DIR)
+            if x.endswith(".wav")
+        }
+        return sfx, music
 
     def get_loaded_sounds(self) -> List[str]:
         """Returns a list of all currently loaded sounds by filename"""
@@ -41,6 +54,9 @@ class Soundboard:
         """
         mixer.find_channel(True).play(self.sfx[name])
 
-    def quit(self) -> None:
-        """Quit the soundboard mixer."""
-        mixer.quit()
+    def play_music(self, name: str) -> None:
+        """Play a file by name, (no extension), and loop it
+
+        :param name: the name of the file in the sfx directory
+        """
+        mixer.Channel(0).play(self.music[name], loops=-1)
